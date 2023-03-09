fb33b2b4fd1803f721af936d9b9a4d4a0451c54d
soohyang136 <jsh515228@gmail.com>
2023-03-10 00:57:07 +0900

    init

---
+++ src/test/java/tool/tool/ToolApplicationTests.java
@@ -0,0 +1,13 @@
+package tool.tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
