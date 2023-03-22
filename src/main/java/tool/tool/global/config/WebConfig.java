374add9b65ca899186eff05d2574c927e7cd03a6
soohyang136 <jsh515228@gmail.com>
2023-03-22 15:06:37 +0900

    ADD :: WebConfig

---
+++ src/main/java/tool/tool/global/config/WebConfig.java
@@ -0,0 +1,23 @@
+package tool.tool.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.PATCH.name(),
+                        HttpMethod.DELETE.name()
+                );
+    }
+}
