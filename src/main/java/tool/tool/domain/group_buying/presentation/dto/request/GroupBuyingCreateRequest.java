5e693d9906c85624d89159d61db335879fa3ab98
soohyang136 <jsh515228@gmail.com>
2023-04-17 13:46:41 +0900

    UPDATE :: 공구 create request validation

--- src/main/java/tool/tool/domain/group_buying/presentation/dto/request/GroupBuyingCreateRequest.java
+++ src/main/java/tool/tool/domain/group_buying/presentation/dto/request/GroupBuyingCreateRequest.java
@@ -1,16 +1,33 @@
 package tool.tool.domain.group_buying.presentation.dto.request;
 
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
 
 @Getter
 public class GroupBuyingCreateRequest {
+    @NotNull
+    @Min(0)
     private Long leaderId;
+
+    @NotBlank
+    @Size(min = 1, max = 20)
     private String title;
+
+    @Min(1)
     private int maxPeople;
+
+    @NotBlank
     private String content;
+
+    @NotBlank
     private String untilAt;
+
+    @NotBlank
     private String category;
+
+    @Min(0)
     private int cost;
+
 }
