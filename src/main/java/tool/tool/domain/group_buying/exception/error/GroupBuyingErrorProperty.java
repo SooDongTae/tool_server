1f2564c94b7f5def3f77e5193986ef2ffa54ece5
soohyang136 <jsh515228@gmail.com>
2023-04-17 12:22:58 +0900

    ADD :: 공구가 꽉 차 있을 시 에러

--- src/main/java/tool/tool/domain/group_buying/exception/error/GroupBuyingErrorProperty.java
+++ src/main/java/tool/tool/domain/group_buying/exception/error/GroupBuyingErrorProperty.java
@@ -7,7 +7,8 @@ import tool.tool.global.error.exception.ErrorProperty;
 @Getter
 @AllArgsConstructor
 public enum GroupBuyingErrorProperty implements ErrorProperty {
-    GROUP_BUYING_NOT_FOUND(404, "해당 공구가 없습니다.");
+    GROUP_BUYING_NOT_FOUND(404, "해당 공구가 없습니다."),
+    PEOPLE_MAX(500, "이미 완성된 공구입니다.");
 
     private final int status;
     private final String message;
