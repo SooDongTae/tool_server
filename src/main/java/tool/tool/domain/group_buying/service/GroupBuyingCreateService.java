015e26c122b11400c0a4828997fd8096c2994d62
soohyang136 <jsh515228@gmail.com>
2023-04-25 09:54:26 +0900

    UPDATE :: 공구 생성 시 이미지 저장

--- src/main/java/tool/tool/domain/group_buying/service/GroupBuyingCreateService.java
+++ src/main/java/tool/tool/domain/group_buying/service/GroupBuyingCreateService.java
@@ -3,13 +3,17 @@ package tool.tool.domain.group_buying.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 import tool.tool.domain.group_buying.domain.GroupBuying;
 import tool.tool.domain.group_buying.facade.GroupBuyingFacade;
 import tool.tool.domain.group_buying.presentation.dto.request.GroupBuyingCreateRequest;
+import tool.tool.domain.image.service.ImageService;
 import tool.tool.domain.user.domain.Leader;
 import tool.tool.domain.user.domain.User;
 import tool.tool.domain.user.facade.LeaderFacade;
 import tool.tool.domain.user.facade.UserFacade;
+
+import java.io.IOException;
 
 @Service
 @RequiredArgsConstructor
@@ -17,12 +21,15 @@ public class GroupBuyingCreateService {
     private final GroupBuyingFacade groupBuyingFacade;
     private final UserFacade userFacade;
     private final LeaderFacade leaderFacade;
+    private final ImageService imageService;
 
     @Transactional
-    public void execute(GroupBuyingCreateRequest request) {
+    public void execute(GroupBuyingCreateRequest request, MultipartFile file) throws IOException {
         User user = userFacade.findUserById(request.getLeaderId());
         Leader leader = leaderFacade.saveLeader(user, request.getBank(), request.getAccount());
         GroupBuying groupBuying = groupBuyingFacade.saveGroupBuying(request, leader);
+        String imgSrc = imageService.saveFile(file);
+        groupBuying.updateImg(imgSrc);
         leader.setGroupBuying(groupBuying);
     }
 }
