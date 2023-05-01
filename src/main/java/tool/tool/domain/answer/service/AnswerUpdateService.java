47c10e72391dc06bb725808893b7dbd00c26efb5
soohyang136 <jsh515228@gmail.com>
2023-05-01 15:33:35 +0900

    UPDATE :: 질문 답변 수정 시 유저 검사

--- src/main/java/tool/tool/domain/answer/service/AnswerUpdateService.java
+++ src/main/java/tool/tool/domain/answer/service/AnswerUpdateService.java
@@ -5,16 +5,21 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import tool.tool.domain.answer.domain.Answer;
 import tool.tool.domain.answer.facade.AnswerFacade;
+import tool.tool.domain.user.domain.User;
+import tool.tool.domain.user.facade.UserFacade;
 
 @Service
 @RequiredArgsConstructor
 public class AnswerUpdateService {
 
     private final AnswerFacade answerFacade;
+    private final UserFacade userFacade;
 
     @Transactional
     public void execute(Long id, String content) {
         Answer answer = answerFacade.findAnswerById(id);
+        User currentUser = userFacade.findUserById(userFacade.getCurrentUser().getId());
+        userFacade.checkUser(currentUser, answer.getUser());
         answer.update(content);
     }
 
