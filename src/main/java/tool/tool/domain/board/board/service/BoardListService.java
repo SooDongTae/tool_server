fb34c4ffbcf0931121cdfab45e938e59a9064ae3
soohyang136 <jsh515228@gmail.com>
2023-05-13 05:07:36 +0900

    FIX :: 리뷰 반영

--- src/main/java/tool/tool/domain/board/board/service/BoardListService.java
+++ src/main/java/tool/tool/domain/board/board/service/BoardListService.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import tool.tool.domain.board.board.domain.Board;
 import tool.tool.domain.board.board.domain.repository.BoardRepository;
 import tool.tool.domain.board.board.presentation.dto.response.BoardListResponse;
@@ -15,14 +16,18 @@ public class BoardListService {
 
     private final BoardRepository boardRepository;
 
+    @Transactional
     public BoardListResponse execute(int size, int page, String title, String category) {
         Pageable pageable = PageRequest.of(page - 1, size);
-        Page<Board> boards;
-        if(category.equals("all")) {
-            boards = boardRepository.findByTitleContaining(title, pageable);
-        } else {
-            boards = boardRepository.findByCategoryAndTitleContaining(category, title, pageable);
-        }
+        Page<Board> boards = getAllOrByCategory(title, category, pageable);
         return BoardListResponse.of(boards);
+    }
+
+    private Page<Board> getAllOrByCategory(String title, String category, Pageable pageable) {
+        if(category.equals("all")) {
+            return boardRepository.findByTitleContaining(title, pageable);
+        } else {
+            return boardRepository.findByCategoryAndTitleContaining(category, title, pageable);
+        }
     }
 }
