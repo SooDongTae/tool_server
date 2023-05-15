386360b558f6681c2e124862be08c6e45186c793
soohyang136 <jsh515228@gmail.com>
2023-05-16 01:05:25 +0900

    FEAT :: delete comment

--- src/main/java/tool/tool/domain/board/comment/service/CommentDeleteService.java
+++ src/main/java/tool/tool/domain/board/comment/service/CommentDeleteService.java
@@ -2,25 +2,24 @@ package tool.tool.domain.board.comment.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import tool.tool.domain.board.comment.domain.Comment;
+import tool.tool.domain.board.comment.domain.repository.CommentRepository;
 import tool.tool.domain.board.comment.facade.CommentFacade;
-import tool.tool.domain.board.comment.presentation.dto.request.CommentUpdateRequest;
 import tool.tool.domain.user.domain.User;
 import tool.tool.domain.user.facade.UserFacade;
 
 @Service
 @RequiredArgsConstructor
-public class CommentUpdateService {
+public class CommentDeleteService {
 
     private final CommentFacade commentFacade;
     private final UserFacade userFacade;
+    private final CommentRepository commentRepository;
 
-    @Transactional
-    public void execute(Long id, CommentUpdateRequest request) {
+    public void execute(Long id) {
         Comment comment = commentFacade.findById(id);
         User currentUser = userFacade.getCurrentUser();
         userFacade.checkUser(currentUser, comment.getUser());
-        comment.update(request);
+        commentRepository.delete(comment);
     }
 }
