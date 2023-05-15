99171f2b23ff4caed1e2b48ecae96d815fbd32ed
soohyang136 <jsh515228@gmail.com>
2023-05-16 00:09:18 +0900

    ADD :: comment repository, controller

---
+++ src/main/java/tool/tool/domain/board/comment/presentation/CommentController.java
@@ -0,0 +1,9 @@
+package tool.tool.domain.board.comment.presentation;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/comment")
+public class CommentController {
+}
