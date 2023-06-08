4a9ff3ee1dc7010ff2b7b4dca3f7b6a03e205af5
soohyang136 <jsh515228@gmail.com>
2023-06-08 11:22:47 +0900

    test

--- src/main/java/tool/tool/global/jpa/QueryFactoryConfig.java
+++ src/main/java/tool/tool/global/jpa/QueryFactoryConfig.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
 @Configuration
 @RequiredArgsConstructor
 public class QueryFactoryConfig {
