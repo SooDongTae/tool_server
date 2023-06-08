0963b52d33a738989cfd03f5ac18d846f96f9856
soohyang136 <jsh515228@gmail.com>
2023-06-08 11:53:13 +0900

    test

---
+++ src/main/generated/tool/tool/domain/question/domain/QQuestion.java
@@ -0,0 +1,68 @@
+package tool.tool.domain.question.domain;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QQuestion is a Querydsl query type for Question
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QQuestion extends EntityPathBase<Question> {
+
+    private static final long serialVersionUID = 1994606610L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QQuestion question = new QQuestion("question");
+
+    public final tool.tool.global.entity.QBaseTimeEntity _super = new tool.tool.global.entity.QBaseTimeEntity(this);
+
+    public final ListPath<tool.tool.domain.answer.domain.Answer, tool.tool.domain.answer.domain.QAnswer> answerList = this.<tool.tool.domain.answer.domain.Answer, tool.tool.domain.answer.domain.QAnswer>createList("answerList", tool.tool.domain.answer.domain.Answer.class, tool.tool.domain.answer.domain.QAnswer.class, PathInits.DIRECT2);
+
+    public final StringPath content = createString("content");
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createdAt = _super.createdAt;
+
+    public final tool.tool.domain.group_buying.domain.QGroupBuying groupBuying;
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final BooleanPath isSecret = createBoolean("isSecret");
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> modifiedAt = _super.modifiedAt;
+
+    public final tool.tool.domain.user.domain.QUser user;
+
+    public QQuestion(String variable) {
+        this(Question.class, forVariable(variable), INITS);
+    }
+
+    public QQuestion(Path<? extends Question> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QQuestion(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QQuestion(PathMetadata metadata, PathInits inits) {
+        this(Question.class, metadata, inits);
+    }
+
+    public QQuestion(Class<? extends Question> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.groupBuying = inits.isInitialized("groupBuying") ? new tool.tool.domain.group_buying.domain.QGroupBuying(forProperty("groupBuying"), inits.get("groupBuying")) : null;
+        this.user = inits.isInitialized("user") ? new tool.tool.domain.user.domain.QUser(forProperty("user"), inits.get("user")) : null;
+    }
+
+}
+
