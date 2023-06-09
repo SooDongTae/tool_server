9d180712a7506f429efff3a7ff9bf598f9018db7
soohyang136 <jsh515228@gmail.com>
2023-06-10 01:07:00 +0900

    update domain

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains="api-tool.kro.kr"
+domains="tool-api.o-r.kr"
 rsa_key_size=4096
 data_path="./data/certbot"
 email="" # Adding a valid address is strongly recommended
